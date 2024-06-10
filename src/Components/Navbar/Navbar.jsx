42b6551c032788d87cc553d148d844709e84ea52
Mert <133760259+PehlivanMert@users.noreply.github.com>
2024-06-11 00:15:54 +0300

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Nav = () => {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav-logo"
       />
-      <img alt="User logged" src="https://i.ibb.co/vzXwwbN/Comfy-UI-00002.png" " className="nav-avatar" />
+      <img alt="User logged" src="https://i.ibb.co/vzXwwbN/Comfy-UI-00002.png"  className="nav-avatar" />
     </nav>
   );
 };
